64c531dd084a7f88f6885f24cfc5a6fde2ec29df
j.ciecierski <jakub.ciecier@gmail.com>
2015-03-22 20:26:21 +0100

    Added stuff

--- color_chooser.py
+++ color_chooser.py
@@ -4,9 +4,30 @@ import six
 
 class ColorChooser:
     def __init__(self):
-        self.colors = []
-        for name, hex in six.iteritems(matplotlib.colors.cnames):
-            self.colors.append(name)
+        # If a color does not look good on the plot, delete it from this list
+        self.colors = ["dimgrey",
+                        "lightslategray",
+                        "lime",
+                        "springgreen",
+                        "red",
+                        "olive",
+                        "firebrick",
+                        "seagreen",
+                        "saddlebrown",
+                        "indigo",
+                        "mediumturquoise",
+                        "crimson",
+                        "darkolivegreen",
+                        "mediumslateblue",
+                        "lightseagreen",
+                        "springgreen",
+                        "lightsalmon",
+                        "darkorchid",
+                        "darkgoldenrod",
+                        "sage",
+                        "salmon"]
+        #for name, hex in six.iteritems(matplotlib.colors.cnames):
+            #self.colors.append(name)
     
     def getColor(self):
         _c = random.choice(self.colors)
